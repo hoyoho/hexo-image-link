076c31c1864188c495cf6440e4027345e051acf5
hoyoho <474844988@qq.com>
2021-12-30 22:26:55 +0800

    修正错误

--- index.js
+++ index.js
@@ -24,20 +24,23 @@ hexo.extend.filter.register('before_post_render', function(data){
 hexo.extend.filter.register('before_post_render', function(data){
 
     data.content = data.content.replace(/<img[\s]*src="([^\[\]]*)"[\s]*alt="([^\[\]]*)"[\s]*style="zoom:[\s]*([1-9][0-9]*)%;"[\s]*\/>/g,
-        function(match_str, path, label){
+        function(match_str, path){
             // if only one /
             if( (path.split("/")).length == 2){
                 console.debug("Html Image Path: " + match_str);
                 console.debug("File Path: " + data.path + " " +
-                              "Matched Path: " + path + " " +
-                              "label: " + label);
-                return match_str.replace(path, "/" + data.path + label);
+                              "Image Path: " + path);
+                return match_str.replace(path, "/" + data.path + (path.split("/"))[1]);
             }else{
-                console.debug("Markdown Image Path does not exists!");
+                console.debug("Html Image Path does not exists!");
                 return match_str;
             }
 
         });
+
+    return data;
+});
+
 
     return data;
 });
